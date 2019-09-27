b1c1196661d063e0c0bdbf83e85eecd147a0c806
Brandon Mogren <brandon.mogren@learnet.se>
2019-09-27 08:45:43 +0200

    Gjorde så att man måste confirma med lösenord när man ska låna en bok

--- src/Library/lana.java
+++ src/Library/lana.java
@@ -8,75 +8,82 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
-public class lana extends JFrame{
-	
+public class lana extends JFrame {
+
 	private SQL conn = new SQL();
 	private String[] temp = new String[30];
 	private JComboBox<String> larare;
 	private JComboBox<String> elev;
-	
+	private final static String password = "Bra123";
+
 	public lana(JFrame parent) {
-		this.setPreferredSize(new Dimension(600,600));
+		this.setPreferredSize(new Dimension(600, 600));
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		this.setLocationRelativeTo(parent);
-		this.setLayout(new GridLayout(2,1));
-		
-		
-		
-		
+		this.setLayout(new GridLayout(2, 1));
+
 		String stmt = "Select concat(larare.fnamn, ' ', larare.enamn) as namn from larare;";
 		Object[][] temp = conn.getData(stmt);
-		
-		for(int i = 0; i < temp.length; i++)
+
+		for (int i = 0; i < temp.length; i++)
 			System.out.println(temp[i][0]);
-		for(int i = 0; i < temp.length; i++) {
-			
+		for (int i = 0; i < temp.length; i++) {
+
 			this.temp[i] = temp[i][0].toString();
-			
+
 		}
-		
+
 		larare = new JComboBox<>(this.temp);
-		
+
 		stmt = "Select concat(elev.fnamn, ' ', elev.enamn) as namn from elev";
 		temp = conn.getData(stmt);
-		
-		for(int i = 0; i < temp.length; i++) {
-			
+
+		for (int i = 0; i < temp.length; i++) {
+
 			this.temp[i] = (String) temp[i][0];
-			
+
 		}
-		
+
 		elev = new JComboBox<>(this.temp);
-		
-		JTextField txt = new JTextField("ID",15);
-		
+
+		JTextField txt = new JTextField("ID", 15);
+
 		JButton send = new JButton("Skicka");
-		
+
 		send.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				int lararID = larare.getSelectedIndex()+1;
-				int elevID = elev.getSelectedIndex()+1;
-				String mediaID = txt.getText();
+
+				String psw = JOptionPane.showInputDialog("Skriv in lösenord");
 				
-				String stmt = "insert into utlanad(larar_ID, elev_ID,media_ID,datum) VALUES("+lararID+","+elevID+",\""+mediaID+" \", CURDATE());";
-				conn.sendData(stmt);
+				if (psw.equals(password)) {
+
+					int lararID = larare.getSelectedIndex() + 1;
+					int elevID = elev.getSelectedIndex() + 1;
+					String mediaID = txt.getText();
+
+					String stmt = "insert into utlanad(larar_ID, elev_ID,media_ID,datum) VALUES(" + lararID + ","
+							+ elevID + ",\"" + mediaID + " \", CURDATE());";
+					conn.sendData(stmt);
+				}else {
+					JOptionPane.showMessageDialog(null, "Prova igen");
+				}
+
 			}
-			
+
 		});
-		
+
 		this.add(larare);
 		this.add(elev);
 		this.add(txt);
 		this.add(send);
-		
+
 		pack();
 		this.setVisible(true);
 	}
-	
-	
-	
+
 }
